df79a312c564d2a9983d40c14b8f5eb8a7bada13
samaamasoud <samaa8206@gmail.com>
2026-05-01 11:59:27 +1000

    week7

---
+++ week 7/script.js
@@ -0,0 +1,36 @@
+// first get access to the audio element so that we can control it from here //
+
+const airportAudio = document.querySelector("#airport-audio");
+console.log(airportAudio);
+// let us access the video too
+const myVideo = document.querySelector("#my-video");
+console.log(myVideo);
+// similarly access the play button
+const playButton = document.querySelector("#play-button");
+console.log(playButton);
+
+playButton.addEventListener("click", playAudio);
+
+function playAudio() {
+  // airportAudio.play();
+}
+
+// similarly access the pause button
+const pauseButton = document.querySelector("#pause-button");
+console.log(pauseButton);
+
+pauseButton.addEventListener("click", pauseAudio);
+
+function pauseAudio() {
+  myVideo.pause();
+  // airportAudio.pause();
+  msg.textContent = "audio is paused";
+}
+
+// similarly access the pop button
+const popAudio = document.querySelector("#pop-audio");
+console.log(popAudio);
+const popButton = document.querySelector("#pop-button");
+console.log(popButton);
+
+popButton.addEventListener("click", makeItPop);
